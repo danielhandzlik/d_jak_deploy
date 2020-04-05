8e28bc5351d5395f5a33f0edcd9952f9dcd56562
danielhandzlik <danielhandzlik@gmail.com>
2020-04-05 19:44:47 +0200

    zadanie 3 daftacademy

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+app.counter = 0
+
+class Patient_Rq(BaseModel):
+    name: str
+    surename: str
+
+class Patient_Resp(BaseModel):
+    id: int
+    patient: Patient_Rq
 
 @app.get('/')
 def path_returns_HelloWorld():
@@ -21,3 +31,8 @@ def returns_put():
 @app.delete('/method')
 def returns_get():
     return {"method": "DELETE"}
+
+@app.post("/patient", response_model=Patient_Resp)
+def visit_patient(rq: Patient_Rq):
+    app.counter += 1
+    return Patient_Resp(id=app.counter,patient=rq)
