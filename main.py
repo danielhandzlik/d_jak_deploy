7fc0424aee5d793d644d419799cc9f38922c8b27
danielhandzlik <danielhandzlik@gmail.com>
2020-04-05 18:36:45 +0200

    zadanie 2 daftacademy

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ app = FastAPI()
 def path_returns_HelloWorld():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.get('/GET)
+@app.get('/GET')
 def returns_get():
     return {"method": "GET"}
 
-@app.post('/POST)
+@app.post('/POST')
 def returns_post():
     return {"method": "POST"}
 
-@app.PUT('/PUT)
+@app.PUT('/PUT')
 def returns_put():
     return {"method": "PUT"}
 
-@app.delete('/DELETE)
+@app.delete('/DELETE')
 def returns_get():
     return {"method": "DELETE"}
